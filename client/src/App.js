2bd590b6830e7eac2d1b1ed71c0c42e6dda83a58
nbrailsford <nbrailsford@msn.com>
2021-08-24 17:59:49 -0600

    added basic components and some style

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import "./App.css";
+import "./styles/App.css";
+import Header from "./Header";
+import AboutGame from "./aboutGame";
+import AboutUs from "./AboutUs";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header />
+      <AboutGame />
+      <AboutUs />
+      <Footer />
     </div>
   );
 }
