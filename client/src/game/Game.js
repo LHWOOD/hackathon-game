55702ffe5eed0d92c6b13b66360d49cb9308c0ff
nbrailsford <nbrailsford@msn.com>
2021-08-25 11:22:24 -0600

    ball functionality

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -2,16 +2,25 @@ import React from "react";
 import "./game.css";
 
 class Game extends React.Component {
-  state = { characterTop: 100 };
+  state = { characterTop: 100, opening: 0, birdPosition: 100 };
+
   componentDidMount() {
     const hole = document.getElementById("hole");
     const character = document.getElementById("character");
 
     hole.addEventListener("animationiteration", () => {
-      let random = -(Math.random() * 300 + 150);
-      console.log(random);
+      let random = -Math.floor(Math.random() * 300 + 150);
+      this.setState({ opening: random });
       hole.style.top = random + "px";
     });
+
+    setInterval(() => {
+      let birdFall = this.state.birdPosition;
+
+      character.style.top = birdFall + "px";
+      this.setState({ birdPosition: -3 });
+      console.log(this.state.birdPosition);
+    }, 10);
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   }
 
