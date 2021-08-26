5e1cbe476cdb7cfe6ff9acf21ae14e544d20c65f
nbrailsford <nbrailsford@msn.com>
2021-08-25 19:43:30 -0600

    red ball falls down

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -2,34 +2,56 @@ import React from "react";
 import "./game.css";
 
 class Game extends React.Component {
-  state = { characterTop: 100, opening: 0, birdPosition: 100 };
+  state = { characterTop: 100, opening: 0, birdPosition: 100, jumping: 0 };
 
   componentDidMount() {
     const hole = document.getElementById("hole");
     const character = document.getElementById("character");
 
+    // creates the hole in a random position
     hole.addEventListener("animationiteration", () => {
       let random = -Math.floor(Math.random() * 300 + 150);
       this.setState({ opening: random });
       hole.style.top = random + "px";
     });
 
+    //gravity simulator
+    let birdFall = this.state.birdPosition;
     setInterval(() => {
-      let birdFall = this.state.birdPosition;
-
-      character.style.top = birdFall + "px";
-      this.setState({ birdPosition: -3 });
-      console.log(this.state.birdPosition);
-    }, 10);
-    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+      if (this.state.jumping === 0) {
+        birdFall++;
+        character.style.top = birdFall + "px";
+      }
+    }, 100);
   }
+
+  //jumping function
+  jump = (event) => {
+    this.setState({ jumping: 1 });
+    let jumpCount = 0;
+    setInterval(() => {
+      let birdJump = this.state.birdPosition;
+      birdJump -= 5;
+      if (jumpCount > 20) {
+        clearInterval(jumpCount);
+        jumpCount = 0;
+        console.log("greater than 20");
+      }
+      jumpCount++;
+      this.setState({ birdPosition: birdJump });
+      this.setState({ jumping: 0 });
+    }, 100);
+  };
 
   render() {
     return (
-      <div id="game">
+      <div id="game" onClick={this.jump}>
         <div id="block"></div>
         <div id="hole"></div>
-        <div id="character"></div>
+        <div
+          id="character"
+          style={{ top: `${this.state.birdPosition}px` }}
+        ></div>
       </div>
     );
   }
