f9f5e7c96d7b9941fa6e32c92d25d4c3e2fcb93a
LHWOOD <hutsonwood@gmail.com>
2021-08-27 12:30:29 -0600

    progress on form

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,39 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/form.css";
 
+// function Signup(props) {
+//   const [formState, setFormState] = useState({
+//     password: "",
+//     username: "",
+//   });
+
 //componentDidMount
+// async function newUserHandler(event) {
+//   event.preventDefault();
+
+//   const username = document.querySelector("#userName").value.trim();
+//   const password = document.querySelector("#password").value.trim();
+
+//   const response = await fetch(`/api/Users`, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       username,
+//       password,
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+
+//   if (response.ok) {
+//     document.location.replace("/game");
+//   } else {
+//     alert("Failed to create account. Check your credentials.");
+//   }
+// }
+
+// document
+//   .querySelector(".create-account")
+//   .addEventListener("submit", newUserHandler);
+
+// class Form extends React.Component {
+//   state = { userinput: "" };
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>Signup</h2>
+//         <form onSubmit={newUserHandler}>
+//           <div>
+//             <label>User Name:</label>
+//             <input
+//               placeholder="User Name"
+//               name="userName"
+//               type="userName"
+//               id="userName"
+//               onChange={handleChange}
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="password">Password:</label>
+//             <input
+//               placeholder="******"
+//               name="password"
+//               type="password"
+//               id="password"
+//               onChange={handleChange}
+//             />
+//           </div>
+//           <div>
+//             <button type="submit">Submit</button>
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 class Form extends React.Component {
-  state = { userinput: "" };
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  async handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+    const name = this.state.value;
+
+    await fetch(`/api/`, {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(name);
+  }
 
   render() {
     return (
-      <div>
-        <h2>Signup</h2>
-        <form>
-          <div>
-            <label>User Name:</label>
-            <input
-              placeholder="User Name"
-              name="userName"
-              type="userName"
-              id="userName"
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input
-              placeholder="******"
-              name="password"
-              type="password"
-              id="password"
-            />
-          </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
