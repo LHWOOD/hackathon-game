cdce1f3fa0df238291e9e4dedfbe18a00673d7a7
LHWOOD <hutsonwood@gmail.com>
2021-08-27 11:28:45 -0600

    updated form with appropriate user fields

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,12 +1,37 @@
 import React from "react";
 
+//componentDidMount
+
 class Form extends React.Component {
   state = { userinput: "" };
 
   render() {
     return (
       <div>
-        <form>hello</form>
+        <h2>Signup</h2>
+        <form>
+          <div>
+            <label>User Name:</label>
+            <input
+              placeholder="User Name"
+              name="userName"
+              type="userName"
+              id="userName"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="password"
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
