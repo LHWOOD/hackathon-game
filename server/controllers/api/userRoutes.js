23c0fbf00892058ccf192cdecb48f109459eabfa
LHWOOD <hutsonwood@gmail.com>
2021-08-28 16:37:50 -0600

    created the remaining crud ops

--- server/controllers/api/userRoutes.js
+++ server/controllers/api/userRoutes.js
@@ -3,12 +3,30 @@ const { User } = require("../../models");
 
 //find all users //tested all good
 router.get("/", (req, res) => {
-  User.findAll({
-    attributes: {
-      exclude: ["password"],
+  User.findAll()
+    .then((userData) => res.json(userData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//get one user //test all good
+router.get("/:id", (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id,
     },
   })
-    .then((userData) => res.json(userData))
+    .then((userData) => {
+      if (!userData) {
+        res.status(404).json({
+          message: "No user found",
+        });
+        return;
+      }
+      res.json(userData);
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -29,6 +47,55 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+//update score //test all good for individual score
+router.put("/:id", (req, res) => {
+  console.log(req);
+  User.update(
+    {
+      score: req.body.score,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((score) => {
+      res.json(score);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//login //tested all good
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((userData) => {
+    if (!userData) {
+      res.status(400).json({
+        message: "No user with that usernam",
+      });
+      return;
+    }
+    res.json({
+      user: userData,
+      message: "You are now logged in",
+    });
+  });
+});
+
+//logout //no go
+router.post("/logout", (req, res) => {
+  req.destroy(() => {
+    res.status(204).end();
+  });
 });
 
 module.exports = router;
